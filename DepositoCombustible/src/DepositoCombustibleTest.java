8a50761c3fad648906754ef42ae0cc82892a7ad5
Luis Manuel Gomez <gomez.luismnl@gmail.com>
2020-01-19 21:13:21 +0100

    Test Comprobar capacidad maxima deposito

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest  {
+
+	
+	/*
+	 * Prueba que compruebe que, utilizando el método correspondiente,
+	 * se obtiene el valor límite de máxima capacidad del depósito
+	 */
+	@Test
+	public void testGetDepositoMax() {
+		DepositoCombustible tank = new DepositoCombustible(45.0,20.0);
+		assertEquals(45, tank.getDepositoMax(),0);
+	}
+
+}
